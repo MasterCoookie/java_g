d6e4d97b64509e6c2591c3c86e5dd5d5d3f73fce
MasterCoookie <jas.kocurek2@wp.pl>
2022-11-11 14:57:48 +0100

    display listing title

--- src/main/java/pl/polsl/ListingController.java
+++ src/main/java/pl/polsl/ListingController.java
@@ -6,7 +6,10 @@ package pl.polsl;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+import pl.polsl.jktab.model.Listing;
 
 /**
  * FXML Controller class
@@ -14,8 +17,19 @@ import javafx.fxml.Initializable;
  * @author Jan
  */
 public class ListingController {
-
+    private final Listing listing;
+    @FXML
+    private Text listingTitle;
     /**
      * Initializes the controller class.
      */
+    public ListingController(Listing _listing) {
+       this.listing = _listing;
+       System.out.println(this.listing.getPrice());
+    }
+    
+    @FXML
+    public void initialize() {
+        this.listingTitle.setText(this.listing.getTitle());
+    }
 }
