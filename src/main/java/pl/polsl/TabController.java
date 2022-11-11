c6745a19c6ff3acea2a558fef7db51878556d464
MasterCoookie <jas.kocurek2@wp.pl>
2022-11-11 12:09:37 +0100

    rowClick def and call

--- src/main/java/pl/polsl/TabController.java
+++ src/main/java/pl/polsl/TabController.java
@@ -13,8 +13,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
 import pl.polsl.jktab.model.Listing;
 import pl.polsl.jktab.model.Tab;
 
@@ -44,6 +46,19 @@ public class TabController {
         listingName.setCellValueFactory(new PropertyValueFactory<Listing, String>("title"));
         listingPrice.setCellValueFactory(new PropertyValueFactory<Listing, Float>("price"));
         postedBy.setCellValueFactory(new PropertyValueFactory<Listing, String>("authorUname"));
+        
+        listingsTable.setRowFactory(rv -> {
+            TableRow<Listing> row = new TableRow();
+            row.setOnMouseClicked(event -> {
+            if (! row.isEmpty() && event.getButton()==MouseButton.PRIMARY 
+                 && event.getClickCount() == 2) {
+
+                Listing clickedRow = row.getItem();
+                rowClick(clickedRow);
+            }
+        });
+        return row ;
+            });
     }
  
     public TabController(Tab _tab) {
@@ -53,5 +68,9 @@ public class TabController {
         this.tab.getListings().forEach(x -> {
             System.out.println(x.getTitle());
         });
+    }
+    
+    private void rowClick(Listing l) {
+        System.out.println(l.getTitle());
     }
 }
