3696b0ace14a78ec30242b488bd28112ad95d757
MasterCoookie <jas.kocurek2@wp.pl>
2022-11-13 17:12:15 +0100

    qr generation somehow functional

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module pl.polsl {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.base;
+    requires qrgen;
 
     opens pl.polsl to javafx.fxml;
     opens pl.polsl.jktab.model to javafx.base;
--- src/main/java/pl/polsl/ListingController.java
+++ src/main/java/pl/polsl/ListingController.java
@@ -4,17 +4,28 @@
  */
 package pl.polsl;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 import pl.polsl.jktab.model.Listing;
+
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+
 
 /**
  * FXML Controller class
@@ -47,7 +58,23 @@ public class ListingController {
         
         DecimalFormat df = new DecimalFormat();
         df.setMaximumFractionDigits(2);
- 
         this.listingPrice.setText(df.format(this.listing.getPrice()));
+    }
+    
+    @FXML
+    public void generateQR() {
+        ByteArrayOutputStream out = QRCode.from("LT Jerry0022").to(ImageType.PNG).withSize(200, 200).stream();
+        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+        
+        Stage stage = new Stage();
+        
+        BorderPane root = new BorderPane();
+        Image image = new Image(in);
+        ImageView view = new ImageView(image);
+        view.setStyle("-fx-stroke-width: 2; -fx-stroke: blue");
+        root.setCenter(view);
+        Scene scene = new Scene(root, 200, 200);
+        stage.setScene(scene);
+        stage.show();
     }
 }
